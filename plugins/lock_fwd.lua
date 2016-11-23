c1f0d43245efefb935feecf3580cd1b09fd45284
X-EvaneScencE-X <amy.IraqI2015@gmail.com>
2016-11-23 12:40:01 +0300

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -1,37 +1,49 @@
+do
+local function pre_process(msg)
 
-do 
+    local fwd = 'mate:'..msg.to.id
+    if redis:get(fwd) and not is_momod(msg) and msg.fwd_from then
+            delete_msg(msg.id, ok_cb, true)
+            return msg
+end
+        return msg
+    end
+    
+ local function mohammed(msg, matches)
+     chat_id = msg.to.id
+local reply_id = msg['id']
+     if is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
+         local fwd = 'mate:'..msg.to.id
+         redis:set(fwd, true)
+         local text = 'تم ☑️ قفل 🔒 اعاده توجيه ✋😽\n🔺Order By : @'..msg.from.username..'\n🔻Order By : '.. msg.from.id..'\n'
+         return reply_msg(reply_id, text, ok_cb, false)
+         end
+local reply_id = msg['id']
+    if not is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
+    local text= 'للـمـشـرفـيـن فـقـط 👮🖕🏿'
+ return reply_msg(reply_id, text, ok_cb, false)
+end
+local reply_id = msg['id']
+if is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
+    local fwd = 'mate:'..msg.to.id
+    redis:del(fwd)
+    local text = 'تم ☑️ فتح 🔓 اعاده توجيه ✋😽\n🔺Order By : @'..msg.from.username..'\n🔻Order By : '.. msg.from.id..'\n'
+    return reply_msg(reply_id, text, ok_cb, false)
+end
 
-local function pre_process(msg) 
-    --Checking mute 
-    local hash = 'mate:'..msg.to.id 
-    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then 
-            delete_msg(msg.id, ok_cb, true) 
-            send_large_msg(get_receiver(msg), 'عزيزي '..msg.from.first_name..'\nممنوع عمل اعادة توجيه من القنوات هنا التزم بقوانين المجموعة 👋👮\n#username @'..msg.from.username) 
-            return "done" 
-        end 
-        return msg 
-    end 
+local reply_id = msg['id']
+if not is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
+local text= ' للـمـشـرفـيـن فـقـط 👮🖕🏿'
+ return reply_msg(reply_id, text, ok_cb, false)
+ end
 
-local function DevPoint(msg, matches) 
-    chat_id =  msg.to.id 
-    if is_momod(msg) and matches[1] == 'قفل' then 
-                    local hash = 'mate:'..msg.to.id 
-                    redis:set(hash, true) 
-                    return "" 
-  elseif is_momod(msg) and matches[1] == 'فتح' then 
-                    local hash = 'mate:'..msg.to.id 
-                    redis:del(hash) 
-                    return "" 
-end 
-
-end 
-
-return { 
-    patterns = { 
-        '^(قفل) اعادة توجيه$', 
-        '^(فتح) اعادة توجيه$' 
-    }, 
-    run = DevPoint, 
-    pre_process = pre_process 
-} 
-end 
+end
+return {
+    patterns ={
+        '^(قفل) (اعادة توجيه)$',
+        '^(فتح) (اعادة توجيه)$'
+    },
+run = mohammed,
+pre_process = pre_process 
+}
+end
